886ad40a0c56fdca13e72dc68ba4ece1adbb0422
V3D1 <maciej.kick@gmail.com>
2024-12-28 20:00:29 +0100

    add some code structure and configuration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TestComponent from '@/components/TestComponent.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <TestComponent />
   </main>
 </template>
